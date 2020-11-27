9daa09ea8cc80b226403ba121a44d702b06a60e9
Marco Antonio Manrique Acha <manriqueacham@gmail.com>
2020-11-27 13:20:49 -0500

    🚀 RELEASE: v.0.0.1b

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://easystoryapi2020.azurewebsites.net/api',
     headers: {
         'Content-type': 'application/json'
     }
